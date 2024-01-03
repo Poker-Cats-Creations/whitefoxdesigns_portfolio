ad1eae09aad751833056f1460d2eae5272fe6e74
Star-Owl <hasiradoo@starowl.dev>
2024-01-03 16:16:14 +0100

    fix

--- src/components/organism/Artwork.jsx
+++ src/components/organism/Artwork.jsx
@@ -11,7 +11,7 @@ const Gallery = async () => {
    return (
       <div className='container mx-auto px-4 py-12'>
          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-            {artworksData.map((artwork) => (
+            {artworksData?.map((artwork) => (
                <div
                   key={artwork.deviationid}
                   className='flex flex-col items-center space-y-6'>
